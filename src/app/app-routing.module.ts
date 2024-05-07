705bf44dd9d6f86a0b26319990884acfbc9dac4f
DegaHariniReddy <harinidega4@gmail.com>
2024-05-07 15:21:13 +0530

    changes by harini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-// import { SalariesComponent } from './salaries/salaries.component';
+import { SalariesComponent } from './Accounts/salaries/salaries.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'Home',component:HomeComponent},
-  // {path:'salaries',component:SalariesComponent}
+  {path:'salaries',component:SalariesComponent}
 ];
 
 @NgModule({
