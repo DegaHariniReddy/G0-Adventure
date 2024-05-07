c8dbd53d75a4a3e8d660b3d932cec9f012506f83
KalakondaNihirika <nihirikakalakonda2000@gmail.com>
2024-05-07 15:19:45 +0530

    Changes by niharika

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OfficeExpensesComponent } from './Accounts/office-expenses/office-expenses.component';
 // import { SalariesComponent } from './salaries/salaries.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'Home',component:HomeComponent},
+  {path:'office expenses',component:OfficeExpensesComponent}
   // {path:'salaries',component:SalariesComponent}
 ];
 
