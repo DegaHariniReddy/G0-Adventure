7d8b47b3c2dab4081a23c25418abdf2812d2eb57
VarlaReddy <varlareddy12@gmail.com>
2024-05-12 14:40:13 +0530

    Change by VNReddy

--- src/app/Accounts/trip-bills/trip-bills.component.ts
+++ src/app/Accounts/trip-bills/trip-bills.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-trip-bills',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./trip-bills.component.css']
 })
 export class TripBillsComponent {
+  tripbillsform!:FormGroup;
+  constructor(private fb:FormBuilder){
+    this.formInit();
+    }
+    formInit(){
+      this.tripbillsform=this.fb.group({
+        billId:[''],
+        transctionId:['', Validators.required],
+        tripId: ['', Validators.required],
+        tripName:['', Validators.required],
+        billAmount: ['', Validators.required],
+        paidAmount:['', Validators.required],
+        dueAmount:['', Validators.required],
+        totalAmount:['', Validators.required],
+        agentId:['', Validators.required],
+        agentName:['', Validators.required],
+        agentType:['', Validators.required],
+      })
 
+    }
 }
